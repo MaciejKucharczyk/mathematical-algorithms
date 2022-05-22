27687fd81a26150fd02c77f44278297825388454
MaciejKucharczyk <maciej.maciek.MK@gmail.com>
2022-05-22 20:01:38 +0200

    fibbonaci bug fixed

--- python/algorithms.py
+++ python/algorithms.py
@@ -1,19 +1,27 @@
-def nwd(a, b)-> int:
-    while b:
-        a, b = b, a%b
-    return a
+class Algorithms:
 
-def fibb(n) -> int:
-    result = 1
-    a0 = 0
-    a1 = 1
-    a_n = 2
-    if(n<=2):
-        return result
-    else:
-        while(a_n!=n):
-            result = a0 + a1
-            a0 = a1
-            a1 = result
-            a_n+=1
-        return result
+    def __init__(self):
+        ""
+
+    def nwd(a, b)-> int:
+        while b:
+            a, b = b, a%b
+        return a
+
+    def fibb(n) -> int:
+        result = 1
+        a0 = 0
+        a1 = 1
+        a_n = 1
+        if(n<1):
+            return result
+        else:
+            while(a_n!=n):
+                result = a0 + a1
+                a0 = a1
+                a1 = result
+                a_n+=1
+            return result
+
+wynik = Algorithms.fibb(3)
+print(wynik)
